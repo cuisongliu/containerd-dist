ea9c8cd6e1d314900189c6a990964f1c1b51442a
cuisongliu <cuisongliu@qq.com>
2019-03-15 16:50:09 +0800

    Update containerd-uninstall.sh

--- containerd-uninstall.sh
+++ containerd-uninstall.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 systemctl stop containerd
 systemctl disable containerd
+rm -rf /etc/systemd/system/containerd.service
 rm -rf /etc/containerd
 rm -rf /etc/crictl.yaml
 rm -rf /var/lib/containerd
